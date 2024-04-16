00495229b460aeac23751a24196740d0147d2ed0
Girish <gdhoundiyal2487@gmail.com>
2024-04-16 17:11:44 +0530

    workingOn

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<script>
+import { ref } from "vue";
+import ChildComponent from "./components/TheHeader.vue";
+import FormComponent from "./components/Form.vue"
+
+
+
+export default {
+  name: "RootComponent",
+  components: {
+    ChildComponent,
+    FormComponent
+  },
+  data() {
+    return {
+      inputvalue : [],
+      items: [],
+    };
+  },
+  methods: {
+    add(event, index) {
+      this.count++;
+      this.myVariable = "helu"
+      this.items.push({
+        id : this.items.length + 1,
+        name: this.inputvalue
+      })
+      this.inputvalue = ''
+      
+    },
+    deleteItem(itemId){
+       this.items =  this.items.filter((item)=> item.id !== itemId)
+        console.log("delete Item", this.items)
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="Root">
+    <!-- <div id="container">
+      <h2 class="TodoHeading">To do List</h2>
+      <input placeholder="Write something" v-model="inputvalue"/>
+      <button @click="(event, index) => add(event, index)" >Add todo</button>
+      <ul class="ListItem">
+      <li class="item" v-for="(item, index) in items" :key="index">
+       <p class="todoText"> {{ item.name }} </p> 
+        <button @click="() => deleteItem(item.id)" class="btn">delete</button>
+      </li>
+    </ul>
+    </div> -->
+    <!-- <ChildComponent  username="hello from Parent" /> -->
+    <FormComponent/>
+  </div>
+</template>
+<style>
+body {
+  margin: 0;
+  background: #f0f0f5;
+}
+.Root {
+  height: auto;
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  align-items: center;
+}
+#container {
+  height: auto;
+  width: 50vw;
+  background: white;
+  text-align: center;
+  padding: 30px;
+}
+.ListItem{
+    list-style: none;
+    padding: 0;
+}
+.item {
+    border: 1px solid black;
+    border-radius: 5px;
+    /* margin: 10px; */
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+.todoText{
+    font-size: 20px;
+}
+.btn{
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,49 @@
+<template>
+  <form>
+    <label for="username"></label>
+    <input type="text" required v-model="username" />
+    <label for="password"></label>
+    <input type="password" required v-model="password" />
+    <button @click="formSubmit()">Submit</button>
+    <button @click="showdetails()">{{ btnText }}</button>
+    <div v-if="isActive">
+        <p>username is {{ userdata.name }}</p>
+        <p>userpass is {{ userdata.pass }}</p>
+    </div>
+  </form>
+</template>
+
+<script>
+console.log("outer")
+export default {
+data(){
+    return{
+        userdata: { name: "",
+    pass: "",},
+    username: "",
+    password: "",
+    isActive: false,
+    isdisabled: true
+    };
+},
+methods: {
+    formSubmit(){
+        console.log("form Submitted", this.username, this.password, this.userdata.name)
+        this.userdata.name = this.username
+        this.userdata.pass = this.password
+        this.username = ""
+        this.password = ""
+        this.btnText = "show Details"
+    },
+    showdetails(){
+        console.log("showdetails")
+        this.isdisabled = false
+        this.isActive = !this.isActive;
+        // !this.isActive ? this.btnText = "hide details" : this.btnText = "Show details" 
+    }
+}
+}
+</script>
+
+<style>
+</style>
